340f9ba06211bf29b60dc6dcb1f360a86dae9ee2
Aniket Gargya <gargya.aniket@gmail.com>
2020-06-30 18:50:43 -0500

    added gravatar hash function

--- mod.ts
+++ mod.ts
@@ -1,7 +1,11 @@
 import { Md5 } from "./deps.ts";
 
-export const getGravatarUrl = (email: string): string => {
+export const getGravatarHash = (email: string): string => {
   const hash = new Md5();
   hash.update(email);
-  return `https://www.gravatar.com/${hash}.json`;
-}
+  return hash.toString();
+};
+
+export const getGravatarUrl = (email: string): string => {
+  return `https://www.gravatar.com/${getGravatarHash(email)}.json`;
+};
--- test.ts
+++ test.ts
@@ -1,9 +1,22 @@
 import { assertEquals } from "./test_deps.ts";
-import { getGravatarUrl } from "./mod.ts";
+import { getGravatarUrl, getGravatarHash } from "./mod.ts";
 
 Deno.test(
-    "Check `Get Gravatar URL`",
-    () => {
-        assertEquals(getGravatarUrl("gargya.aniket@gmail.com"), "https://www.gravatar.com/15d4fc7c2997c7869b3a855fa01cb6d4.json");
-    }
+  "Check `Get Gravatar Hash`",
+  () => {
+    assertEquals(
+      getGravatarHash("gargya.aniket@gmail.com"),
+      "15d4fc7c2997c7869b3a855fa01cb6d4",
+    );
+  },
+);
+
+Deno.test(
+  "Check `Get Gravatar URL`",
+  () => {
+    assertEquals(
+      getGravatarUrl("gargya.aniket@gmail.com"),
+      "https://www.gravatar.com/15d4fc7c2997c7869b3a855fa01cb6d4.json",
+    );
+  },
 );
